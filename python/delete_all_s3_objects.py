f966ed0febdedba05b328f56b2f1a5728703b36a
cloud303-kloucks <kloucks@cloud303.io>
2023-10-17 15:17:20 -0700

    added python script to effectively delete millions of objects

---
+++ python/delete_all_s3_objects.py
@@ -0,0 +1,20 @@
+import boto3
+
+def delete_all_objects(bucket_name):
+    s3 = boto3.client('s3')
+
+    # Get the list of objects in the bucket
+    paginator = s3.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=bucket_name)
+    total_deleted = 0
+    for page in pages:
+        keys_to_delete = [{'Key': obj['Key']} for obj in page['Contents']]
+        s3.delete_objects(Bucket=bucket_name, Delete={'Objects': keys_to_delete})
+        print(f'Deleted {page["KeyCount"]} objects')
+        total_deleted = total_deleted + page["KeyCount"]
+        print(f'Total objects deleted: {total_deleted}')
+
+
+if __name__ == '__main__':
+    bucket_name = 'forecast-prod-us-east-1-benchmarklabs-partitioned'  # Replace with your S3 bucket name
+    delete_all_objects(bucket_name)
